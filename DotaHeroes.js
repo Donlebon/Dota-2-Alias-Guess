0f1182dde94a32b8c3020af9ec4d212334a9f0f5
Don <donducdo@gmail.com>
2022-09-10 00:00:50 -0700

    Finished Project

---
+++ DotaHeroes.js
@@ -0,0 +1,500 @@
+let DotaHeroes = [{
+    localized_name: "Ancient Apparition",
+    picture: "AncientApparation.jpg",
+    alt_name: "Kaldr",
+    primary_attribute: "Intelligence"
+}, {
+    localized_name: "Bane",
+    picture: "Bane.jpg",
+    alt_name: "Atropos"
+ },
+ {
+    localized_name: "Batrider",
+    picture: "Batrider.jpg",
+    alt_name: "Batrider"
+},{
+    localized_name: "Chen",
+    picture: "Chen.jpg",
+    alt_name: "Holy Knight"
+},{
+    localized_name: "Crystal Maiden",
+    picture: "CrystalMaiden.jpg",
+    alt_name: "Rylai"
+},{
+    localized_name: "Dark Seer",
+    picture: "DarkSeer.jpg",
+    alt_name: "Ish Kafel"
+},{
+    localized_name: "Dark Willow",
+    picture: "DarkWillow.jpg",
+    alt_name: "Mireska Sunbreeze"
+},{
+    localized_name: "Dazzle",
+    picture: "Dazzle.jpg",
+    alt_name: "Shadow Priest"
+},{
+    localized_name: "Death Prophet",
+    picture: "DeathProphet.jpg",
+    alt_name: "Krobelus"
+},{
+    localized_name: "Disruptor",
+    picture: "Disruptor.jpg",
+    alt_name: "Stormcrafter"
+},{
+    localized_name: "Enchantress",
+    picture: "Enchantress.jpg",
+    alt_name: "Aiushtha"
+},{
+    localized_name: "Enigma",
+    picture: "Enigma.jpg",
+    alt_name: "Consumer of Worlds"
+},{
+    localized_name: "Grimstroke",
+    picture: "Grimstroke.jpg",
+    alt_name: "Grimstroke"
+},{
+    localized_name: "Invoker",
+    picture: "Invoker.jpg",
+    alt_name: "Carl"
+},{
+    localized_name: "Jakiro",
+    picture: "Jakiro.jpg",
+    alt_name: "Twin Head Dragon"
+},{
+    localized_name: "Keeper of the Light",
+    picture: "KeeperoftheLight.jpg",
+    alt_name: "Ezalor"
+},{
+    localized_name: "Leshrac",
+    picture: "Leshrac.jpg",
+    alt_name: "Tormented Soul"
+},{
+    localized_name: "Lich",
+    picture: "Lich.jpg",
+    alt_name: "Ethreain"
+},{
+    localized_name: "Lina",
+    picture: "Lina.jpg",
+    alt_name: "Slayer"
+},{
+    localized_name: "Lion",
+    picture: "Lion.jpg",
+    alt_name: "Demon Witch"
+},{
+    localized_name: "Nature Prophet",
+    picture: "NatureProphet.jpg",
+    alt_name: "Furion"
+},{
+    localized_name: "Necrophos",
+    picture: "Necrophos.jpg",
+    alt_name: "Rotundjere"
+},{
+    localized_name: "Ogre Magi",
+    picture: "OgreMagi.jpg",
+    alt_name: "Aggron Stonebreak"
+},{
+    localized_name: "Oracle",
+    picture: "Oracle.jpg",
+    alt_name: "Nerif"
+},{
+    localized_name: "Outworld Destroyer",
+    picture: "OutworldDestroyer.jpg",
+    alt_name: "Harbinger"
+},{
+    localized_name: "Puck",
+    picture: "Puck.jpg",
+    alt_name: "Faerie Dragon"
+},{
+    localized_name: "Pugna",
+    picture: "Pugna.jpg",
+    alt_name: "Pugna"
+},{
+    localized_name: "Queen of Pain",
+    picture: "QueenofPain.jpg",
+    alt_name: "Akasha"
+},{
+    localized_name: "Rubick",
+    picture: "Rubick.jpg",
+    alt_name: "Grand Magus"
+},{
+    localized_name: "Shadow Demon",
+    picture: "ShadowDemon.jpg",
+    alt_name: "Shadow Demon"
+},{
+    localized_name: "Shadow Shaman",
+    picture: "ShadowShaman.jpg",
+    alt_name: "Rhasta"
+},{
+    localized_name: "Silencer",
+    picture: "Silencer.jpg",
+    alt_name: "Nortrom"
+},{
+    localized_name: "Skywrath Mage",
+    picture: "SkywrathMage.jpg",
+    alt_name: "Dragonus"
+},{
+    localized_name: "Storm Spirit",
+    picture: "StormSpirit.jpg",
+    alt_name: "Raijin Thunderkeg"
+},{
+    localized_name: "Techies",
+    picture: "Techies.jpg",
+    alt_name: "Squee Spleen and Sploon"
+},{
+    localized_name: "Tinker",
+    picture: "Tinker.jpg",
+    alt_name: "Boush"
+},{
+    localized_name: "Visage",
+    picture: "Visage.jpg",
+    alt_name: "Bound Form of Necrolic"
+},{
+    localized_name: "Void Spirit",
+    picture: "VoidSpirit.jpg",
+    alt_name: "Inai"
+},{
+    localized_name: "Warlock",
+    picture: "Warlock.jpg",
+    alt_name: "Demnok Lannik"
+},{
+    localized_name: "Windranger",
+    picture: "Windranger.jpg",
+    alt_name: "Lyralei"
+},{
+    localized_name: "Winter Wyvern",
+    picture: "WinterWyvern.jpg",
+    alt_name: "Auroth"
+},{
+    localized_name: "Witch Doctor",
+    picture: "WitchDoctor.jpg",
+    alt_name: "Zharvakko"
+},{
+    localized_name: "Zeus",
+    picture: "Zeus.jpg",
+    alt_name: "Lord of Heaven"
+}, {
+    localized_name: "Anti Mage",
+    picture: "AntiMage.jpg",
+    alt_name: "Magina",
+    primary_attribute: "agility"
+}, {
+    localized_name: "Arc Warden",
+    picture: "ArcWarden.jpg",
+    alt_name: "Zet"
+},{
+    localized_name: "Bloodseeker",
+    picture: "Bloodseeker.jpg",
+    alt_name: "Strygwyr"
+},{
+    localized_name: "Bounty Hunter",
+    picture: "BountyHunter.jpg",
+    alt_name: "Gondar"
+},{
+    localized_name: "Broodmother",
+    picture: "Broodmother.jpg",
+    alt_name: "Black Arachnia"
+},{
+    localized_name: "Clinkz",
+    picture: "Clinkz.jpg",
+    alt_name: "Bone Fletcher"
+},{
+    localized_name: "Drow Ranger",
+    picture: "Drowranger.jpg",
+    alt_name: "Traxex"
+},{
+    localized_name: "Ember Spirit",
+    picture: "EmberSpirit.jpg",
+    alt_name: "Xin"
+},{
+    localized_name: "Faceless Void",
+    picture: "FacelessVoid.jpg",
+    alt_name: "Darkterror"
+},{
+    localized_name: "Gyrocopter",
+    picture: "Gyrocopter.jpg",
+    alt_name: "Aurel"
+},{
+    localized_name: "Hoodwink",
+    picture: "Hoodwink.jpg",
+    alt_name: "Hoodwink"
+},{
+    localized_name: "Juggernaut",
+    picture: "Juggernaut.jpg",
+    alt_name: "Yurnero"
+},{
+    localized_name: "Lone Druid",
+    picture: "LoneDruid.jpg",
+    alt_name: "Sylla"
+},{
+    localized_name: "Luna",
+    picture: "Luna.jpg",
+    alt_name: "Moon Rider"
+},{
+    localized_name: "Medusa",
+    picture: "Medusa.jpg",
+    alt_name: "Gorgon"
+},{
+    localized_name: "Meepo",
+    picture: "Meepo.jpg",
+    alt_name: "Geomancer"
+},{
+    localized_name: "Mirana",
+    picture: "Mirana.jpg",
+    alt_name: "Princess of the Moon"
+},{
+    localized_name: "Monkey King",
+    picture: "MonkeyKing.jpg",
+    alt_name: "Sun Wukong"
+},{
+    localized_name: "Morphling",
+    picture: "Morphling.jpg",
+    alt_name: "Morphling"
+},{
+    localized_name: "Naga Siren",
+    picture: "NagaSiren.jpg",
+    alt_name: "Slithice"
+},{
+    localized_name: "Nyx Assassin",
+    picture: "NyxAssassin.jpg",
+    alt_name: "Anubarak"
+},{
+    localized_name: "Pangolier",
+    picture: "Pangolier.jpg",
+    alt_name: "Donte Panlin"
+},{
+    localized_name: "Phantom Assassin",
+    picture: "PhantomAssassin.jpg",
+    alt_name: "Mortred"
+},{
+    localized_name: "Phantom Lancer",
+    picture: "PhantomLancer.jpg",
+    alt_name: "Azwraith"
+},{
+    localized_name: "Razor",
+    picture: "Razor.jpg",
+    alt_name: "Lightning Revenant"
+},{
+    localized_name: "Riki",
+    picture: "Riki.jpg",
+    alt_name: "Stealth Assassin"
+},{
+    localized_name: "Shadow Fiend",
+    picture: "ShadowFiend.jpg",
+    alt_name: "Nevermore"
+},{
+    localized_name: "Slark",
+    picture: "Slark.jpg",
+    alt_name: "Nightcrawler"
+},{
+    localized_name: "Sniper",
+    picture: "Sniper.jpg",
+    alt_name: "Kardel Sharpeye"
+},{
+    localized_name: "Spectre",
+    picture: "Spectre.jpg",
+    alt_name: "Mercurial"
+},{
+    localized_name: "Templar Assassin",
+    picture: "TemplarAssassin.jpg",
+    alt_name: "Lanaya"
+},{
+    localized_name: "Terrorblade",
+    picture: "Terrorblade.jpg",
+    alt_name: "Demon Marauder"
+},{
+    localized_name: "Troll Warlord",
+    picture: "TrollWarlord.jpg",
+    alt_name: "Jahrakal"
+},{
+    localized_name: "Ursa Warrior",
+    picture: "UrsaWarrior.jpg",
+    alt_name: "Ulfsaar"
+},{
+    localized_name: "Vengeful Spirit",
+    picture: "VengefulSpirit.jpg",
+    alt_name: "Shendelzare"
+},{
+    localized_name: "Venomancer",
+    picture: "Venomancer.jpg",
+    alt_name: "Lesale Deathbringer"
+},{
+    localized_name: "Viper",
+    picture: "Viper.jpg",
+    alt_name: "Netherdrake"
+},{
+    localized_name: "Weaver",
+    picture: "Weaver.jpg",
+    alt_name: "Skitskurr"
+}, {
+    localized_name: "Abaddon",
+    picture: "Abaddon.jpg",
+    alt_name: "Lord of Avernus",
+    primary_attribute: "strength"
+}, {
+    localized_name: "Alchemist",
+    picture: "Alchemist.jpg",
+    alt_name: "Razzil Darkbrew"
+},{
+    localized_name: "Axe",
+    picture: "Axe.jpg",
+    alt_name: "Mogul Khan"
+},{
+    localized_name: "Beastmaster",
+    picture: "Beastmaster.jpg",
+    alt_name: "Karroch"
+},{
+    localized_name: "Brewmaster",
+    picture: "Brewmaster.jpg",
+    alt_name: "Mangix"
+},{
+    localized_name: "Bristleback",
+    picture: "Bristleback.jpg",
+    alt_name: "Rigwarl"
+},{
+    localized_name: "Centaur Warrunner",
+    picture: "CentaurWarrunner.jpg",
+    alt_name: "Bradwarden"
+},{
+    localized_name: "Chaos Knight",
+    picture: "ChaosKnight.jpg",
+    alt_name: "Armageddon"
+},{
+    localized_name: "Clockwerk",
+    picture: "Clockwerk.jpg",
+    alt_name: "Rattletrap"
+},{
+    localized_name: "Dawnbreaker",
+    picture: "Dawnbreaker.jpg",
+    alt_name: "Valora"
+},{
+    localized_name: "Doom",
+    picture: "Doom.jpg",
+    alt_name: "Lucifer"
+},{
+    localized_name: "Dragon Knight",
+    picture: "DragonKnight.jpg",
+    alt_name: "Davion"
+},{
+    localized_name: "Earth Spirit",
+    picture: "EarthSpirit.jpg",
+    alt_name: "Kaolin"
+},{
+    localized_name: "Earthshaker",
+    picture: "Earthshaker.jpg",
+    alt_name: "Raigor Stonehoof"
+},{
+    localized_name: "Elder Titan",
+    picture: "ElderTitan.jpg",
+    alt_name: "Worldsmith"
+},{
+    localized_name: "Huskar",
+    picture: "Huskar.jpg",
+    alt_name: "Sacred Warrior"
+},{
+    localized_name: "Io",
+    picture: "Io.jpg",
+    alt_name: "Wisp"
+},{
+    localized_name: "Kunkka",
+    picture: "Kunkka.jpg",
+    alt_name: "Admiral"
+},{
+    localized_name: "Legion Commander",
+    picture: "LegionCommander.jpg",
+    alt_name: "Tresdin"
+},{
+    localized_name: "Lifestealer",
+    picture: "Lifestealer.jpg",
+    alt_name: "Naix"
+},{
+    localized_name: "Lycan",
+    picture: "Lycan.jpg",
+    alt_name: "Banehallow"
+},{
+    localized_name: "Magnus",
+    picture: "Magnus.jpg",
+    alt_name: "Magnoceros"
+},{
+    localized_name: "Marci",
+    picture: "Marci.jpg",
+    alt_name: "Marci"
+},{
+    localized_name: "Mars",
+    picture: "Mars.jpg",
+    alt_name: "First Son of Heaven"
+},{
+    localized_name: "Night Stalker",
+    picture: "NightStalker.jpg",
+    alt_name: "Balanar"
+},{
+    localized_name: "Omniknight",
+    picture: "Omninight.jpg",
+    alt_name: "Purist Thunderwrath"
+},{
+    localized_name: "Phoenix",
+    picture: "Phoenix.jpg",
+    alt_name: "Phoenix"
+},{
+    localized_name: "Primal Beast",
+    picture: "PrimalBeast.jpg",
+    alt_name: "Primal Beast"
+},{
+    localized_name: "Pudge",
+    picture: "Pudge.jpg",
+    alt_name: "Butcher"
+},{
+    localized_name: "Sand King",
+    picture: "SandKing.jpg",
+    alt_name: "Crixalis"
+},{
+    localized_name: "Slardar",
+    picture: "Slardar.jpg",
+    alt_name: "Slithereen Guard"
+},{
+    localized_name: "Snapfire",
+    picture: "Snapfire.jpg",
+    alt_name: "Beatrix"
+},{
+    localized_name: "Spirit Breaker",
+    picture: "SpiritBreaker.jpg",
+    alt_name: "Barathrum"
+},{
+    localized_name: "Sven",
+    picture: "Sven.jpg",
+    alt_name: "Rogue Knight"
+},{
+    localized_name: "Tidehunter",
+    picture: "Tidehunter.jpg",
+    alt_name: "Leviathan"
+},{
+    localized_name: "Timbersaw",
+    picture: "Timbersaw.jpg",
+    alt_name: "Rizzrack"
+},{
+    localized_name: "Tiny",
+    picture: "Tiny.jpg",
+    alt_name: "Stone Giant"
+},{
+    localized_name: "Treant Protector",
+    picture: "TreantProtector.jpg",
+    alt_name: "Rooftrellen"
+},{
+    localized_name: "Tusk",
+    picture: "Tusk.jpg",
+    alt_name: "Ymir"
+},{
+    localized_name: "Underlord",
+    picture: "Underlord.jpg",
+    alt_name: "Vrogros"
+},{
+    localized_name: "Undying",
+    picture: "Undying.jpg",
+    alt_name: "Almighty Dirge"
+},{
+    localized_name: "Wraith King",
+    picture: "WraithKing.jpg",
+    alt_name: "Ostarion"
+}
+]
+
+export default DotaHeroes
---
+++ index.js
@@ -0,0 +1,400 @@
+import DotaHeroes from "./DotaHeroes.js"
+let mainBackground = document.querySelector(".background")
+let changeBackground = document.querySelector(".change")
+let prevCount = 0
+
+// Generate Random Background Image 
+
+let backgroundImages = ["kunkka.jpg", "es.jpg", "dr.jpg", "lina.jpg", "nyx.jpg", "pudge.jpg", 
+"rubick.jpg", "ss.jpg", "wr.jpg"]
+
+function animateButton(){
+    changeBackground.classList.toggle("translate")
+}
+
+function generateRandomNumber(){
+    let randomGenerator = Math.floor(Math.random() * backgroundImages.length)
+    return randomGenerator
+}
+
+changeBackground.addEventListener("click", function(){
+    let randomNumber = generateRandomNumber()
+    if (randomNumber === prevCount){
+        if (randomNumber === 0){
+            randomNumber += 1
+        }
+        else{
+            randomNumber -= 1
+        }
+    }
+    mainBackground.style.background = `linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url('background-images/${backgroundImages[randomNumber]}')`
+    mainBackground.style.backgroundSize = "cover";
+    mainBackground.style.backgroundPosition = "center";
+    prevCount = randomNumber
+    animateButton()
+})
+
+// Generate Random Blank Letters 
+
+let wordGuess = document.querySelector(".word-guess")
+
+function randomHero(data){
+    return Math.floor(Math.random() * data.length)
+}
+
+let level = document.querySelector(".level")
+let gamelevel = 1;
+
+let localHero = ""
+let hero = ""
+let allDotaHeroes
+
+function getDotaHero(DotaHeroes){
+    localHero = DotaHeroes[randomHero(DotaHeroes)]
+    hero = localHero.alt_name.replaceAll("-", " ")
+}
+
+getDotaHero(DotaHeroes)
+
+function resetAllHeroes(){
+    allDotaHeroes = DotaHeroes.map(item => {
+    return item
+})}
+
+resetAllHeroes()
+
+function removeDefeatedHero(){
+    allDotaHeroes.splice(allDotaHeroes.indexOf(localHero), 1)
+}
+
+let heroArray = []
+
+function getHero(){
+    for(let i = 0; i < hero.length; i++){
+        if(hero[i] === " "){
+            heroArray.push("!")
+        }
+        else{
+            heroArray.push("_")
+        }
+    } 
+}
+
+getHero()
+
+let heroGuess = heroArray.map(item => {
+    if(item === "!"){
+        return `<h1 class = "nextLine"></h1>`
+    } else{
+        return `<h1>${item}</h1>`
+    }
+})
+
+// Display Hero
+
+let hints = document.querySelector(".hints")
+let audio = new Audio("")
+let heroIcon = document.querySelector(".hero-icon")
+let iconSound = document.querySelector(".hero-sound")
+let hitPoints = document.querySelector(".hitpoints")
+
+
+function addPicandSound(){
+    hints.innerHTML = `
+    <img src="/hero-pic/${localHero.localized_name}.webp" alt="Hero-Picture" class = "hero-icon">
+    <img src="icon-sound.png" alt="sound-icon" class = "hero-sound">
+    `
+    audio = new Audio(`/hero-sounds/${localHero.localized_name}.mp3.mpeg`)
+    heroIcon = document.querySelector(".hero-icon")
+    iconSound = document.querySelector(".hero-sound")
+    iconSound.addEventListener("click", function(){
+        audio.play()
+    })
+
+    heroIcon.addEventListener("click", function(){
+        audio.play()
+    })
+}
+
+function displayHero(){
+    wordGuess.innerHTML = `${heroGuess.join("")}`
+    addPicandSound()
+}
+    
+displayHero()
+
+let guessHero = hero.toUpperCase()
+
+// Keyboard Function
+
+let keysList = []
+
+let keys = document.querySelectorAll(".key")
+
+let title = document.querySelector(".title")
+let lifePercentage = 100
+let currentLife = 8
+let lifeTotal = document.querySelector(".lifetotal")
+let healthBar = document.querySelector(".healthbar")
+let trivia = document.querySelector(".trivia")
+
+let header = document.querySelector(".header")
+let defeat = document.createElement("img")
+defeat.classList.add("defeat")
+defeat.src = "Defeat.gif"
+
+let playAgain = document.createElement("button")
+playAgain.classList.add("playAgain")
+playAgain.textContent = "Play Again"
+
+function addKeys(){
+    for(let i = 0; i < keys.length; i++){
+        keys[i].addEventListener("click", testing.bind(null, i))
+    }
+}
+
+function testing(i, e){
+    let test = [...guessHero.matchAll(keys[i].textContent)]
+    let indexes = test.map(match => match.index)
+    if(guessHero.includes(keys[i].textContent)){
+        guessedLetter(indexes)
+        keys[i].classList.add("correctLetter")
+        if(!heroArray.includes("_")){
+            getMeme(generateMemeNumber())
+            displayWinningScreen()
+            wordGuess.innerHTML = `
+            ${hero}
+            `
+            removeDefeatedHero()
+            if(allDotaHeroes.length === 0){
+                mainBackground.innerHTML = `<h1 class = "finishGame">You Win the Game!</h1>`
+                enableAllKeys()
+                disableallKeys()
+            }
+         }
+    } else if(!guessHero.includes(keys[i].textContent)){
+        disableButton(e)
+        keys[i].classList.add("incorrectLetter")
+        decreaseLife()
+    }
+}
+
+addKeys()
+
+function guessedLetter(indexes){
+    for(let i = 0; i < indexes.length; i++){
+        heroArray[indexes[i]] = guessHero[indexes[i]]
+    }
+    let guessingLetter = heroArray.map(item => {
+        if(item === "!"){
+            return `<h1 class = "nextLine"></h1>`
+        } else if (item === "_"){
+            return "<h1>_</h1>"
+        } else{
+            return `<h1>${item}</h1>`
+        }
+    })
+    wordGuess.innerHTML = `${guessingLetter.join("")}`
+}
+
+function decreaseLife(){
+    healthBar.classList.add("healthTransition")
+    currentLife--
+    lifePercentage-=12.5
+    healthBar.style.width = (lifePercentage - 12.5) + "%"
+    lifeTotal.textContent = currentLife + "/8"
+    if(currentLife > 3 && currentLife < 6){
+        healthBar.style.backgroundColor = "#FEC260"
+    } else if (currentLife > 1 && currentLife < 4){
+        healthBar.style.backgroundColor = "#F32424"
+    }
+    if(currentLife === 0){
+        lifeTotal.textContent = "8/8"
+        healthBar.style.backgroundColor = "#1E2022"
+        displayLosingScreen()
+        wordGuess.innerHTML = `
+        ${hero}
+        `
+    }
+}
+
+function displayLosingScreen(){
+    let losingSound = new Audio("LosingSound.mpeg");
+    losingSound.play();
+    title.textContent = "You have fed to " + localHero.localized_name
+    hitPoints.remove()
+    newHitPoints.remove() 
+    mainBackground.insertBefore(defeat, trivia)
+    mainBackground.insertBefore(playAgain, trivia)
+    wordGuess.style.marginTop = "1.5em";
+    disableallKeys()
+    resetGame()
+}
+
+
+function disableButton(e){
+    e.composedPath()[0].disabled = true
+}
+
+function disableallKeys(){
+    for(let i = 0; i < keys.length; i++){
+        keys[i].disabled = true
+    }
+}
+
+function enableAllKeys(){
+    for(let i = 0; i < keys.length; i++){
+        keys[i].disabled = false
+        keys[i].style.backgroundColor = ""
+        keys[i].classList.remove("correctLetter")
+        keys[i].classList.remove("incorrectLetter")
+    }
+}
+
+
+function resetGame(){
+    playAgain.addEventListener("click", loseReset)
+}
+
+// Generate New Hero from Defeat
+
+let victory = document.createElement("img")
+victory.classList.add("defeat")
+
+let newHitPoints = document.createElement("section")
+newHitPoints.classList.add("hitpoints")
+newHitPoints.innerHTML = `<div class = "healthbar">
+<h1 class = "lifetotal">8/8</h1>
+</div>`
+
+function loseReset(){   
+    gamelevel = 1
+    level.textContent = "Level 1"
+    resetAllHeroes()
+    defeat.remove()
+    playAgain.remove()
+    title.textContent = "Dota Alias Trivia"
+    mainBackground.insertBefore(newHitPoints, trivia)
+    enableAllKeys()
+    resetKeys()
+    lifeTotal = document.querySelector(".lifetotal")
+    healthBar = document.querySelector(".healthbar")
+    trivia = document.querySelector(".trivia")
+    killCount = 0
+    healthBar.style = ""
+    lifePercentage = 100
+    currentLife = 8
+    localHero = ""
+    hero = ""
+    getDotaHero(DotaHeroes)
+    heroArray = []
+    getHero()
+    heroGuess = heroArray.map(item => {
+        if(item === "!"){
+            return `<h1 class = "nextLine"></h1>`
+        } else{
+            return `<h1>${item}</h1>`
+        }
+    })
+    displayHero()
+    guessHero = hero.toUpperCase()
+    keysList = []
+    keys = document.querySelectorAll(".key")
+    addKeys()
+    wordGuess.style.marginTop = "";
+    console.log(DotaHeroes)
+}
+
+function resetKeys(){
+    for(let i = 0; i < keys.length; i++){
+        keys[i].outerHTML = keys[i].outerHTML
+}}
+
+// Generate New Hero from Victory
+
+let nextLevel = document.createElement("button")
+nextLevel.classList.add("nextLevel")
+nextLevel.textContent = "Next Opponent"
+
+let killCount = 0
+
+let killStreak = ["1stkill.mp3.mpeg", "2ndkill.mp3.mpeg","3rdkill.mp3.mpeg","4thkill.mp3.mpeg",
+"5thkill.mp3.mpeg","6thkill.mp3.mpeg","7thkill.mp3.mpeg","8thkill.mp3.mpeg","9thkill.mp3.mpeg",
+"10thkill.mp3.mpeg","11thkill.mp3.mpeg"]
+
+function killSound(){
+    if(killCount > 9){
+        let winningSound = new Audio(`/killing-sound/${killStreak[10]}`);
+        winningSound.play();
+    }
+    else if (killCount <= 9){
+        let winningSound = new Audio(`/killing-sound/${killStreak[killCount]}`);
+        winningSound.play();
+    }
+}
+
+function displayWinningScreen(){
+    lifeTotal.textContent = "8/8"
+    killSound()
+    killCount++
+    title.textContent = "You have defeated " + localHero.localized_name
+    hitPoints.remove() 
+    newHitPoints.remove() 
+    mainBackground.insertBefore(victory, trivia)
+    mainBackground.insertBefore(nextLevel, trivia)
+    wordGuess.style.marginTop = "1.5em";
+    disableallKeys()
+    winResetGame()
+}
+
+function winResetGame(){
+    nextLevel.addEventListener("click", winReset)
+}
+
+function winReset(){
+    gamelevel++
+    level.textContent = "Level" + " " + gamelevel
+    victory.remove()
+    nextLevel.remove()
+    title.textContent = "Next Challenger"
+    mainBackground.insertBefore(newHitPoints, trivia)
+    enableAllKeys()
+    resetKeys()
+    lifeTotal = document.querySelector(".lifetotal")
+    healthBar = document.querySelector(".healthbar")
+    trivia = document.querySelector(".trivia")
+    healthBar.style = ""
+    lifePercentage = 100
+    currentLife = 8
+    localHero = ""
+    hero = ""
+    getDotaHero(allDotaHeroes)
+    heroArray = []
+    getHero()
+    heroGuess = heroArray.map(item => {
+        if(item === "!"){
+            return `<h1 class = "nextLine"></h1>`
+        } else{
+            return `<h1>${item}</h1>`
+        }
+    })
+    displayHero()
+    guessHero = hero.toUpperCase()
+    keysList = []
+    keys = document.querySelectorAll(".key")
+    addKeys()
+    wordGuess.style.marginTop = "";
+    console.log(allDotaHeroes)
+}
+
+function generateMemeNumber(){
+    let randomMemeNumber = Math.round(Math.random() * 110)
+    return randomMemeNumber
+}
+
+async function getMeme(memeNumber){
+    let res = await fetch("https://api.gfycat.com/v1/gfycats/search?search_text=win&count=120")
+    let data = await res.json()
+    victory.src = data.gfycats[memeNumber].gif100px
+}
+
